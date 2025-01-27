0ad6033d5d4ffec165efae92c68bbb7e57d6bd63
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-27 13:20:47 -0300

    Starts from beginning

--- main.py
+++ main.py
@@ -3,20 +3,11 @@ import customtkinter
 
 customtkinter.set_appearance_mode("dark")
 
+
 app = customtkinter.CTk()
 app.title("EncrypTexT")
 app.geometry("300x300")
 
-main_menu_label = customtkinter.CTkLabel(app, text="Main Menu")
-main_menu_label.pack(pady=10)
 
-binary_option_button = customtkinter.CTkButton(app, text="Binary")
-binary_option_button.pack(pady=5)
-
-hexadecimal_option_button = customtkinter.CTkButton(app, text="Hexadecimal")
-hexadecimal_option_button.pack(pady=5)
-
-zenit_polar_option_button = customtkinter.CTkButton(app, text="ZENIT POLAR")
-zenit_polar_option_button.pack(pady=5)
 
 app.mainloop()
