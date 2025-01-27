564a5209d0399d7610a6f1388ef0d2419d16bd4f
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-27 17:43:00 -0300

    Adds border radius to the buttons

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = customtkinter.CTk()
 app.title("EncrypTexT")
 app.geometry("500x300")
 
-tabview = customtkinter.CTkTabview(app, width=400)
+tabview = customtkinter.CTkTabview(app, width=400, corner_radius=20)
 tabview.pack()
 
 tabview.add("Binary")
@@ -19,10 +19,10 @@ tabview.tab("Hexadecimal").grid_columnconfigure(0, weight=1)
 tabview.add("ZENIT POLAR")
 tabview.tab("ZENIT POLAR").grid_columnconfigure(0, weight=1)
 
-binary_encrypt_button = customtkinter.CTkButton(tabview.tab("Binary"), text="Encrypt")
+binary_encrypt_button = customtkinter.CTkButton(tabview.tab("Binary"), text="Encrypt", corner_radius=10)
 binary_encrypt_button.pack(pady=50)
 
-binary_decrypt_button = customtkinter.CTkButton(tabview.tab("Binary"), text="Decrypt")
+binary_decrypt_button = customtkinter.CTkButton(tabview.tab("Binary"), text="Decrypt", corner_radius=10)
 binary_decrypt_button.pack()
 
 hexadecimal_encrypt_button = customtkinter.CTkButton(tabview.tab("Hexadecimal"), text="Encrypt")
