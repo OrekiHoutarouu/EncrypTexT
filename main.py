2b966dbcb610ee673b24b5b4f329c4bc0c39e47e
Samuel Campelo <samuelcampelo25@gmail.com>
2025-01-27 17:38:58 -0300

    Creates main page

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 from modules import encrypt, decrypt, utilities
-import customtkinter
+import customtkinter 
 
 customtkinter.set_appearance_mode("dark")
 
-
 app = customtkinter.CTk()
 app.title("EncrypTexT")
-app.geometry("300x300")
+app.geometry("500x300")
 
+tabview = customtkinter.CTkTabview(app, width=400)
+tabview.pack()
 
+tabview.add("Binary")
+tabview.tab("Binary").grid_columnconfigure(0, weight=1)
 
-app.mainloop()
+tabview.add("Hexadecimal")
+tabview.tab("Hexadecimal").grid_columnconfigure(0, weight=1)
+
+tabview.add("ZENIT POLAR")
+tabview.tab("ZENIT POLAR").grid_columnconfigure(0, weight=1)
+
+binary_encrypt_button = customtkinter.CTkButton(tabview.tab("Binary"), text="Encrypt")
+binary_encrypt_button.pack(pady=50)
+
+binary_decrypt_button = customtkinter.CTkButton(tabview.tab("Binary"), text="Decrypt")
+binary_decrypt_button.pack()
+
+hexadecimal_encrypt_button = customtkinter.CTkButton(tabview.tab("Hexadecimal"), text="Encrypt")
+hexadecimal_encrypt_button.pack(pady=50)
+
+hexadecimal_decrypt_button = customtkinter.CTkButton(tabview.tab("Hexadecimal"), text="Decrypt")
+hexadecimal_decrypt_button.pack()
+
+zenit_polar_encrypt_button = customtkinter.CTkButton(tabview.tab("ZENIT POLAR"), text="Encrypt")
+zenit_polar_encrypt_button.pack(pady=50)
+
+zenit_polar_decrypt_button = customtkinter.CTkButton(tabview.tab("ZENIT POLAR"), text="Decrypt")
+zenit_polar_decrypt_button.pack()
+
+app.mainloop() 
